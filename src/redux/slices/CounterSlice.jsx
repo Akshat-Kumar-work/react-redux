ee41a79b9db172dffc54444a1d270efd8d49417b
Akshat <iamakku0.0.0.1@gmail.com>
2023-04-11 21:47:31 +0530

    commiting on 11 april 23

--- src/redux/slices/CounterSlice.jsx
+++ src/redux/slices/CounterSlice.jsx
@@ -5,16 +5,24 @@ const initialState = {
 }
 
 //slice create kari hai createslice method se , aur slice ko export krdia hai
+//CounterSlice ek variable hai jo store krta hai object jo return hoty hai createslice function se
 export const CounterSlice = createSlice({
     //slice name
+   // In Redux, a prefix is a string that is added to the beginning of an action type to 
+  //  create a unique identifier for the action. The prefix helps to prevent naming collisions between actions defined in different parts of your application.
+  //action type k agy use hota hai as a prefix jo differentiate krega actions m 
     name:"counter",
+    
     //an object containing initial state value
     initialState, 
 
+    //The reducers field in a slice is essentially a collection of these reducer functions that are combined together to create a larger reducer for the slice.
     reducers:{
+        // reducer function and action creator 
         increment:(state)=>{
             state.value+=1;
         },
+        // reducer function and action creator 
         decrement:(state)=>{
             state.value-=1;
         }
@@ -27,10 +35,10 @@ export const CounterSlice = createSlice({
 //agar sare reducers function ki implementation ko slice m se bhar nikalana hai toh actions se niklengy
 
 //2 chize hamesha export krni hai
-
-//function ki implementation ko export kar rhe hai
+//CounterSlice.actions is an object that contains the action creators for the slice, including increment and decrement.
+//reducer function ki implementation ko export kar rhe hai , yaha hum function ko export kar rhe hai
 export const {increment, decrement} = CounterSlice.actions;
 
 
-//reducer ko export kar rhe hai
+//CounterSlice.reducer is the actual reducer function that updates the state, 
 export default CounterSlice.reducer;
